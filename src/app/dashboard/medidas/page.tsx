a29b2f4e142da33c0ecb1ce266341a4ef46f7020
rick0376 <seu-email@dominio.com>
2025-11-03 20:42:43 -0300

    medidas list

--- src/app/dashboard/medidas/page.tsx
+++ src/app/dashboard/medidas/page.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
-import MedidasClient from "./MedidasClient";
+//import MedidasClient from "./MedidasClient";
+import MedidasClient from "@/components/listas/medidas/index";
 
 export default function MedidasPage() {
   return (
