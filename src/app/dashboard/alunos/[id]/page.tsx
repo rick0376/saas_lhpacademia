c45ee27d52c2e7e32f56c5903e139846dd9a3db6
rick0376 <seu-email@dominio.com>
2025-12-14 08:02:06 -0300

    first commit

--- src/app/dashboard/alunos/[id]/page.tsx
+++ src/app/dashboard/alunos/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { MedidasList } from "@/components/medidas/MedidasList";
+import { MedidasList } from "@/components/medidas/detalhes/MedidasList";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import styles from "./styles.module.scss";
